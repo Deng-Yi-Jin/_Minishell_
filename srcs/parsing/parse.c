d9fe8e302532cf398a434dbc78d57b86bd82d3f1
root <root@DESKTOP-N98IFBE>
2023-11-23 17:28:58 +0800

    done parsing

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -70,32 +70,6 @@ void	parse(t_token **tokens)
 			(*tokens) = (*tokens)->next;
 		}
 	}
-	// while ((*tokens)->cmd != NULL)
-	// {
-	// 	if ((*tokens)->prev == NULL && (*tokens)->type != PIPE)
-	// 	{
-	// 		(*ast)->next_child = create_ast_node(ft_strdup((*tokens)->cmd));
-	// 		(*ast)->next_child->parent = (*ast);
-	// 		(*ast) = (*ast)->next_child;
-	// 		(*tokens) = (*tokens)->next;
-	// 	}
-	// 	if ((*tokens)->type != PIPE && (*tokens)->cmd != NULL)
-	// 	{
-	// 		(*ast)->next_grandchild = create_ast_node(ft_strdup((*tokens)->cmd));
-	// 		(*ast)->next_grandchild->prev_grandchild = (*ast);
-	// 		(*ast) = (*ast)->next_grandchild;
-	// 	}
-	// 	else if ((*tokens)->type == PIPE)
-	// 	{
-	// 		(*tokens) = (*tokens)->next;
-	// 		(*ast) = ast_first_last((*ast), true, false);
-	// 		(*ast)->next_child = create_ast_node(ft_strdup((*tokens)->cmd));
-	// 		(*ast)->next_child->parent = (*ast)->parent;
-	// 		(*ast)->next_child->prev_child = (*ast);
-	// 		(*ast) = (*ast)->next_child;
-	// 	}
-	// 	(*tokens) = (*tokens)->next;
-	// }
 	(*ast) = ast_first_last(*ast, true, true);
 	print_ast_all(ast);
 	free_ast(ast);
