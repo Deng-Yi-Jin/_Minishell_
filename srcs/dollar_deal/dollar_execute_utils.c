f2e1e32338632b8372dd9e6205452c8bdb0bdf19
Lucas <denglucasyijin@gmail.com>
2024-03-12 08:33:58 +0800

    pushing to hub

--- srcs/dollar_deal/dollar_execute_utils.c
+++ srcs/dollar_deal/dollar_execute_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dollar_execute_utils.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: geibo <geibo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 04:03:10 by codespace         #+#    #+#             */
-/*   Updated: 2024/03/11 05:30:01 by codespace        ###   ########.fr       */
+/*   Updated: 2024/03/11 18:04:32 by geibo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 t_ast	*dollar_execute(t_ast *ast, t_token *command)
 {
 	t_ast	*current_node;
+	char	**cmd;
+	int		i;
 
+	i = 0;
+	cmd = ft_calloc(2, sizeof(char *));
 	current_node = ast->child;
 	while (current_node)
 	{
 		if (current_node->type == CMD)
 		{
-			command = add_tokens(command, ft_strdup(current_node->cmd), CMD);
-			if (current_node->next == NULL)
-				break;
-			current_node = current_node->next;
+			cmd[i] = ft_strdup(current_node->cmd);
+			i++;
 		}
-		else if (current_node->type == DOLLAR)
-			current_node = dollar_execute(current_node->child, command);
+		
 	}
 }
