ea60cc91ca6dac669fa82335bf12d06e58e6bc9e
Kylie <hello.kylieeeee@gmail.com>
2024-09-08 11:36:09 +0800

    fixed(cd): mem bleak fixed

--- srcs/executor/cd.c
+++ srcs/executor/cd.c
@@ -6,7 +6,7 @@
 /*   By: kytan <kytan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:27:48 by kytan             #+#    #+#             */
-/*   Updated: 2024/09/04 16:27:49 by kytan            ###   ########.fr       */
+/*   Updated: 2024/09/08 11:35:46 by kytan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ bool	execute_cd(char *args[N_ARGS], char **envp)
 	char	*path;
 	char	*ms_path;
 	char	*tmp;
+	char	*cwd;
 
-	printf("Executing cd...\n");
 	if (args[1] == NULL || !ft_strcmp(args[1], "~"))
 		path = ft_strdup(getenv("HOME"));
 	else
@@ -27,9 +27,9 @@ bool	execute_cd(char *args[N_ARGS], char **envp)
 		perror_color("cd");
 	modify_env_vars("PWD", getcwd(NULL, 0));
 	ms_path = ft_malloc(PATH_MAX);
-	printf("ms_path [%p]\n", ms_path);
-	ft_snprintf(ms_path, PATH_MAX, "PWD=%s", getcwd(NULL, 0));
-	printf("ms_path [%p]\n", ms_path);
+	cwd = getcwd(NULL, 0);
+	ft_snprintf(ms_path, PATH_MAX, "PWD=%s", cwd);
+	free(cwd);
 	if ((ft_strcmp(find_env_vars("PWD_MALLOC")->value, "1") == 0))
 	{
 		tmp = envp[find_env("PWD", envp)];
