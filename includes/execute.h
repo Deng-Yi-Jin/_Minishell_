9b0d76d6788851e81b8fe3c9f9574a87a4350f9d
Lucas <denglucasyijin@gmail.com>
2024-10-03 18:43:30 +0800

    fix: norm at execute.h

--- includes/execute.h
+++ includes/execute.h
@@ -6,7 +6,7 @@
 /*   By: geibo <geibo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 12:54:15 by codespace         #+#    #+#             */
-/*   Updated: 2024/10/02 15:59:08 by geibo            ###   ########.fr       */
+/*   Updated: 2024/10/03 18:42:37 by geibo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include "parse.h"
 # define N_ARGS 2560
-# include <stddef.h>
 # include <stdbool.h>
+# include <stddef.h>
 
 typedef struct s_exec
 {
@@ -105,10 +105,10 @@ void				child_infd_setup(int infile_fd);
 pid_t				create_fork(void);
 void				dup2_error(void);
 void				close_fd(int *previous_fd, int *infile_fd);
-bool	handle_file_descriptor(int *fd, int (*get_fd_func)(char **),
-		t_exec *exec);
-int	count_cmd_line(t_exec *exec, int i);
-void	allocate_cmd_list(t_exec *exec, int cmd_line);
-void	copy_cmd_to_list(t_exec *exec);
+bool				handle_file_descriptor(int *fd, int (*get_fd_func)(char **),
+						t_exec *exec);
+int					count_cmd_line(t_exec *exec, int i);
+void				allocate_cmd_list(t_exec *exec, int cmd_line);
+void				copy_cmd_to_list(t_exec *exec);
 
 #endif
